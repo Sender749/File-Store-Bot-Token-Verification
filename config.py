1cf1ae63d4dbae6f8894d108a4d465afaa421bc8
Sender749 <jotmahal2003@gmail.com>
2024-08-12 10:37:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -77,7 +77,7 @@ TUT_VID = os.environ.get("TUT_VID","https://t.me/hott_cheks/3")
 
 #Payment to remove the token system
 #put TRUE if you want this feature
-USE_PAYMENT = True if (os.environ.get("USE_PAYMENT", "FALSE") == "TRUE") & (USE_SHORTLINK) else False
+USE_PAYMENT = True if (os.environ.get("USE_PAYMENT", "FALSE") == "FALSE") & (USE_SHORTLINK) else False
 #UPI ID
 UPI_ID = os.environ.get("UPI_ID", "")
 #UPI QR CODE IMAGE
